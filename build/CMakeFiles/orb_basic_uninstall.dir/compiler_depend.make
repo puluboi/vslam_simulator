75e18a30af2bc8a4248de6b1306b50988ffae6c9
puluboi <leo.juhoilmari.rinne@gmail.com>
2025-12-09 17:29:23 +0200

    hmm

--- build/CMakeFiles/orb_basic_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for orb_basic_uninstall.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/puluboi/Documents/projects/orb_basic
-# Build directory: /home/puluboi/Documents/projects/orb_basic/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
